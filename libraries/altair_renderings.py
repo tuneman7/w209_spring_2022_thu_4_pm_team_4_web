1a918c8727e64f6a515886852721ab8fdbb9cbb4
tuneman7 <tuneman7@hotmail.com>
2022-03-06 12:19:02 -0800

    dealio

--- libraries/altair_renderings.py
+++ libraries/altair_renderings.py
@@ -1359,10 +1359,10 @@ class AltairRenderings:
         return my_chart
 
     def china_trade_war_slide_four(self,width=250,height=180):
-        brazil  = self.get_altaire_line_chart_county_trade_for_matrix("China","Brazil")
-        brazil_b    = self.get_altaire_dual_pie_chart_by_types_for_matrix("China","Brazil", "exports")
-        australia  = self.get_altaire_line_chart_county_trade_for_matrix("China","Brazil")
-        australia_b    = self.get_altaire_dual_pie_chart_by_types_for_matrix("China","Brazil", "exports")
+        brazil  = self.get_altaire_line_chart_county_trade_for_matrix("China","Brazil",width=width,height=height)
+        brazil_b    = self.get_altaire_dual_pie_chart_by_types_for_matrix("China","Brazil", "exports",width=width,height=height)
+        australia  = self.get_altaire_line_chart_county_trade_for_matrix("China","Australia",width=width,height=height)
+        australia_b    = self.get_altaire_dual_pie_chart_by_types_for_matrix("China","Australia", "exports",width=width,height=height)
         row_1  = (brazil | brazil_b ).resolve_scale(
             color='independent')
         row_3  = (australia | australia_b ).resolve_scale(
@@ -1375,4 +1375,3 @@ class AltairRenderings:
 
         return my_chart
 
-        
