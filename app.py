9902a96408eeaa0246b818d6c295939c2e7f14e7
tuneman7 <tuneman7@hotmail.com>
2022-03-26 12:05:30 -0700

    https://github.com/tuneman7/w209_spring_2022_thu_4_pm_team_4_web/issues/32

--- app.py
+++ app.py
@@ -577,11 +577,16 @@ def ajaxfile():
     source_country = "United States"
     target_country = "World"
     if request.method == 'POST':
-        source_country = request.form["source_country"]
-        target_country = request.form["target_country"]
+        source_country = request.form["source_country"].strip()
+        target_country = request.form["target_country"].strip()
+    print("source_country=", source_country)
+    print("target_country=", target_country)
     if target_country.lower() == "world":        
         title = "Trade between <b>" + source_country + "</b> and <b>" + target_country + "</b> top 20 trading nations. To see trade with another country select from drop-down: "
+        print("source_country='{}'".format(source_country))
+        print("target_country=", target_country)
         chart_json = my_altair.get_charts_for_click_from_world_map(source_country,width=350,height=200).to_json()
+        #chart_json = my_altair.get_charts_for_click_from_world_map("Russia",width=350,height=200).to_json()
         form = CountryToWorldVisualizationFormWithWorld(request.form,current_source_country=source_country) 
         return jsonify({'htmlresponse': render_template('modal/modal_chart.html',visualization_form=None,chart_json = chart_json,form=form,source_country=source_country,current_target_country=target_country,country_list=None,modal_title=escape(title))})
 
