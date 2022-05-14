36743d99dd86fc3e4ba222b5e22ef07b25bf3d47
tuneman7 <tuneman7@hotmail.com>
2022-05-14 15:01:37 -0700

    dealio

--- setup_on_ubuntu_docker.sh
+++ setup_on_ubuntu_docker.sh
@@ -2,7 +2,7 @@ cd /w209_spring_2022_thu_4_pm_team_4_web
 chmod -R 777 ./*.*
 python3 -m venv myproj
 ./myproj/bin/activate
-./ir.sh
+./ir.sh 
 ./rd.sh &
-sleep 35
+sleep 15
 jupyter notebook --no-browser --ip=0.0.0.0 --allow-root 
