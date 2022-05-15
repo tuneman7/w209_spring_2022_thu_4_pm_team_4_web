61b83dfbc9d83b9f494fdfe04f31ec777505ad42
tuneman7 <tuneman7@hotmail.com>
2022-05-14 19:42:02 -0700

    dealio

--- setup_on_ubuntu_docker.sh
+++ setup_on_ubuntu_docker.sh
@@ -2,6 +2,12 @@ cd /w209_spring_2022_thu_4_pm_team_4_web
 git pull
 chmod -R 777 ./*.*
 ./rd.sh &
-sleep 15
+sleep 25
+echo "**************************"
+echo "**************************"
+echo "*  ACCESS FLASK APP AT:  *"
+echo "* http://127.0.0.1:5001  *"
+echo "**************************"
+echo "**************************"
 cd /
 jupyter notebook --no-browser --ip=0.0.0.0 --allow-root 
