ce474a2c27f43e962fddcf7dd5cc9f1da70ca762
tuneman7 <tuneman7@hotmail.com>
2022-06-01 17:21:51 -0700

    Periodic Update

--- build_and_run_209_docker.sh
+++ build_and_run_209_docker.sh
@@ -1,2 +1,17 @@
-. build_209_web_docker.sh
-docker run -p 8888:8888 -p 5001:5000 w209_proj
+#!/bin/bash
+IMAGE_NAME=w209_proj_don_irwin
+APP_NAME=w209_proj_don_irwin
+DOCKER_FILE=Dockerfile.209proj
+
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+
+echo "docker stop ${APP_NAME}"
+docker stop ${APP_NAME}
+echo "docker rm ${APP_NAME}"
+docker rm ${APP_NAME}
+
+#build docker from the docker file
+echo "docker build -t ${IMAGE_NAME} -f ${DOCKER_FILE}"
+docker build -t ${IMAGE_NAME} -f ${DOCKER_FILE} .
+echo "docker run -d --name ${APP_NAME} -p  8888:8888 -p 5001:5000  ${IMAGE_NAME} "
+docker run -d --name ${APP_NAME} -p  8888:8888 -p 5001:5000  ${IMAGE_NAME} 
