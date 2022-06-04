7b18d3b639a3ea8ba02d8c26a8055d3051c87eba
tuneman7 <tuneman7@hotmail.com>
2022-06-04 00:40:08 -0700

    periodic update

--- build_and_run_209_docker.sh
+++ build_and_run_209_docker.sh
@@ -11,7 +11,7 @@ echo "docker rm ${APP_NAME}"
 docker rm ${APP_NAME}
 
 #build docker from the docker file
-echo "docker build -t ${IMAGE_NAME} -f ${DOCKER_FILE}"
-docker build -t ${IMAGE_NAME} -f ${DOCKER_FILE} .
+echo "docker build -t ${IMAGE_NAME} -f ${DOCKER_FILE}" 
+docker build -t ${IMAGE_NAME} -f ${DOCKER_FILE} . --no-cache
 echo "docker run --name ${APP_NAME} -p  8888:8888 -p 5001:5000  ${IMAGE_NAME} "
 docker run --name ${APP_NAME} -p  8888:8888 -p 5001:5000  ${IMAGE_NAME} 
