96da6621b8006fc0fc3344c59f0f1ba982cadd2b
don <tuneman7>
2023-08-22 18:15:03 -0700

    periodic update

--- setup_on_ubuntu_docker.sh
+++ setup_on_ubuntu_docker.sh
@@ -11,4 +11,4 @@ echo "* http://127.0.0.1:5001  *"
 echo "**************************"
 echo "**************************"
 cd /app
-jupyter notebook --no-browser --ip=0.0.0.0 --allow-root 
+jupyter notebook --no-browser --ip=0.0.0.0 --allow-root --NotebookApp.token="" --NotebookApp.password="datavis"
